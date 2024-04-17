16f3ea68fb07327f19533f5807c38f06b1eec12c
anthonypoon12 <anthony@poons.us>
2024-04-17 19:57:53 -0400

    Finished hw

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ rm output.txt output2.txt
 make all
 ./s1 ImagesForHW4/sphere0.pgm 89 output.txt
 ./s2 output.txt ImagesForHW4/sphere1.pgm ImagesForHW4/sphere2.pgm ImagesForHW4/sphere3.pgm output2.txt
-./s3 output2.txt ImagesForHW4/sphere1.pgm ImagesForHW4/sphere2.pgm ImagesForHW4/sphere3.pgm 10 87 output_normals.pgm output_albedo.pgm
+./s3 output2.txt ImagesForHW4/sphere1.pgm ImagesForHW4/sphere2.pgm ImagesForHW4/sphere3.pgm 15 87 output_normals.pgm output_albedo.pgm
+# ./s3 output2.txt ImagesForHW4/object1.pgm ImagesForHW4/object2.pgm ImagesForHW4/object3.pgm 10 87 output_normals.pgm output_albedo.pgm
 make clean
